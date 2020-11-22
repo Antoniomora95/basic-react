3c686e4d8039fb4500f467ed6bdfab2306c6ac4a
Antonio Bautista <Antonio.Bautista84@t-mobile.com>
2020-11-21 19:50:55 -0600

    fetch API, and use effect hook, clean the view also

--- src/sections/FetchAPI.js
+++ src/sections/FetchAPI.js
@@ -1,10 +1,15 @@
+import './FetchAPI.css';
 import React, { useState, useEffect } from 'react';
 
 const getBitcoinInfo = async () => {
     try {
         const bitcoinResponse = await fetch('https://api.coindesk.com/v1/bpi/currentprice/MXN.json');
         const json = await bitcoinResponse.json();
-        return json;
+        const bitcoinPriceList = cleanResponse(json);
+        return {
+            hasError: false,
+            bitcoinPriceList
+        }
     } catch (error) {
         return {
             hasError: true,
@@ -20,32 +25,46 @@ const cleanResponse = (data) => {
     }
     return currenciesList;
 }
+
+const stringifyError = error => {
+    if(error instanceof Error) {
+        return error.toString();
+    } else {
+        return JSON.stringify(error);
+    }
+}
+
+const RenderError = ({errorDescription}) => <div className='FetchApiContainer RenderError'> { stringifyError(errorDescription) } </div>
+
+const ListCurrencies = ({bitcoinPriceList})  => {
+   return  bitcoinPriceList.map(_currency => {
+        const { rate, description } = _currency;
+        return <div className='FetchApiContainer RenderCurrencies'  key={description}>1 bitcoin is: {rate} {description}</div>
+    })
+}
+
 export default function FetchAPI(props) {
-    const [state, setStateComponent] = useState({ bitcoinPriceList: [], hasError: false, errorDescription: '', loading: false });
+    const [state, setStateComponent] = useState({ bitcoinPriceList: [], hasError: false, errorDescription: 'nothing'});
 
     useEffect(() => {
-        setStateComponent({ loading: true });
         getBitcoinInfo().then(data => {
             if (data.hasError) {
                 const { hasError, errorDescription } = data;
-                setStateComponent({ loading: false, hasError, errorDescription });
+                setStateComponent({hasError, errorDescription });
             } else {
-                setStateComponent({ bitcoinPriceList: cleanResponse(data) })
+                const {bitcoinPriceList} = data;
+                setStateComponent({ bitcoinPriceList })
             }
         });
     }, [])
-    console.log(state, 'sr');
-    const { bitcoinPriceList } = state
+    const { bitcoinPriceList, hasError, errorDescription } = state;
     return (
         <div>
-            <h4>Fetch API example</h4>
-            { bitcoinPriceList && bitcoinPriceList.length &&
-                bitcoinPriceList.map(_currency => {
-                    const { rate, description } = _currency;
-                    return <div key={description}>1 bitcoin is: {rate} {description}</div>
-                })
-            }
+            <h4>Fetch API example </h4>
+             { hasError && <RenderError errorDescription = { errorDescription } /> }
+             { !hasError && <ListCurrencies bitcoinPriceList = { bitcoinPriceList }/> }
         </div>
     );
 }
+    
 
