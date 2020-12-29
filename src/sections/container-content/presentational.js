af09235bc4a2baadeaf4002078b5bf15779e1f84
Antonio Bautista <Antonio.Bautista84@t-mobile.com>
2020-12-29 10:40:36 -0600

    separate the curency item

--- src/sections/container-content/presentational.js
+++ src/sections/container-content/presentational.js
@@ -1,8 +1,11 @@
 export const RenderError = ({ errorDescription }) => <div className='FetchApiContainer RenderError'> { errorDescription } </div>
 
+const CurrencyItem = ({rate, description})=> {
+    return <div className='FetchApiContainer RenderCurrencies'>1 bitcoin is: {rate} {description}</div>
+}
 export const ListCurrencies = ({bitcoinPriceList})  => {
    return  bitcoinPriceList.map(_currency => {
         const { rate, description } = _currency;
-        return <div className='FetchApiContainer RenderCurrencies'  key={description}>1 bitcoin is: {rate} {description}</div>
+        return <CurrencyItem rate = {rate} description = {description} key={description}/>
     })
 }
